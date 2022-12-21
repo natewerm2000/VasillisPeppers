9c7a636c8eced614ce7d36145ae93c7306642daa
natewerm2000 <89500945+natewerm2000@users.noreply.github.com>
2022-12-21 00:23:30 -0500

    import font awesome

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,18 @@
 import companyLogo from './images/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook} from '@fortawesome/free-brands-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar(){
     return <nav className = "nav">
-        <a href = "/" className = "site-title"><img src={companyLogo} alt="BigCo Inc. logo"/></a>
+        <a href = "/" className = "site-title"><img src={companyLogo} alt=""/></a>
         <ul>
             <li>
-                <a href = "/shop">Shop</a>
+                <a href = "/shop">Shop<FontAwesomeIcon icon = {faCartShopping}/></a>
             </li>
             <li>
-                <a href = "/contact">Contact</a>
+                <a href = "/contact">Contact<FontAwesomeIcon icon = {faAddressCard}/></a>
             </li>
         </ul>
     </nav>
