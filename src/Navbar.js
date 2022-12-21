3d23137f4e87d4a5b41354307a19a90e44df100e
natewerm2000 <89500945+natewerm2000@users.noreply.github.com>
2022-12-20 23:44:17 -0500

    navbar logo and colors

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,7 @@
+import companyLogo from './images/logo.png';
 export default function Navbar(){
     return <nav className = "nav">
-        <a href = "/" className = "site-title"> Naate</a>
+        <a href = "/" className = "site-title"><img src={companyLogo} alt="BigCo Inc. logo"/></a>
         <ul>
             <li>
                 <a href = "/shop">Shop</a>
