65111a0e2dfd064aa2300dbffc79cbc86e4b48f3
natewerm2000 <89500945+natewerm2000@users.noreply.github.com>
2022-12-21 01:32:43 -0500

    shop

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,6 +1,22 @@
+import jelly from '../images/logo.png';
+
 export default function Shop(){
-    return <div>
-        <h1>Shop</h1>
-        <h2>This is the shop</h2>
+    return <div className = "shop">
+<div id = "header"><h1>Shop</h1></div>
+    <div class="card">
+        <img src={jelly} alt=""/>
+        <div class="container">
+            <h4><b>Hot Pepper Jelly</b></h4> 
+            <p>dsahdkwjkdhaklwdhdlhwadkwajhd</p> 
+        </div>
     </div>
+    <div id = "space"></div>
+    <div class="card">
+        <img src={jelly} alt=""/>
+        <div class="container">
+            <h4><b>Hot Pepper Seasonings</b></h4> 
+            <p>dsahdkwjkdhaklwdhdlhwadkwajhd</p> 
+        </div>
+    </div>
+</div>
 }
